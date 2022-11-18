de7f36ec555995ff8dda24e4367cd6271ad36190
Joshua Searles <jsearles@plex.com>
2022-11-18 14:02:02 -0500

    chore: add app

---
+++ src/LocalTimeTypeConverter.App/Models/ExampleModel.cs
@@ -0,0 +1,8 @@
+﻿using NodaTime;
+
+namespace LocalTimeTypeConverter.App.Models;
+
+public class ExampleModel
+{
+    public LocalTime Time { get; set; }
+}
---
+++ src/LocalTimeTypeConverter.App/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using LocalTimeTypeConverter.App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocalTimeTypeConverter.App.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public ExampleModel? Example { get; set; }
+
+    [BindProperty]
+    public string? Message { get; set; }
+
+    public IActionResult OnPost()
+    {
+        if (!this.ModelState.IsValid)
+        {
+            this.Message = "Model is invalid";
+            return this.Page();
+        }
+
+        this.Message = $"Time bound to model as {this.Example?.Time.ToString()}";
+        return this.Page();
+    }
+}
